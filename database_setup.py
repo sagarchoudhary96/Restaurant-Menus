f2845512295242a8042c28c7902d43de72075f7c
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-01-28 21:33:09 +0530

    Fix: error due to sqlalchemy import statement

--- database_setup.py
+++ database_setup.py
@@ -1,10 +1,8 @@
 import os
 import sys
-from sqlalchemy import
-Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 
-from sqlalchemy.ext.declarative import
-declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import relationship
 
