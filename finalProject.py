4e2a96742736b242c6d8234994cc8b664f0287f8
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-02-07 03:34:31 +0530

    update: header to show login button

--- finalProject.py
+++ finalProject.py
@@ -84,6 +84,7 @@ def gconnect():
         return response
 
     # Store the access token in the session for later use.
+    login_session['provider'] = "google"
     login_session['credentials'] = credentials
     login_session['gplus_id'] = gplus_id
 
